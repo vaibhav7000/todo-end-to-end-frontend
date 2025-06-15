7b8da546f12979dd7f4108f1417bd378ad8ad723
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-15 16:50:45 +0530

    react todo application

--- src/App.jsx
+++ src/App.jsx
@@ -16,3 +16,15 @@ function App() {
 export default App;
 
 // React was introduced to get rid of the DOM manipulations things
+
+// React is used to create dynamic websites without developer caring about the DOM manipulation, the developer should provide the updated data to the react, react internally calcualtes the difference and performs the DOM manipulations.
+
+// the developer only cares about creating the components that react will render and provides us the website
+
+// React provides us special syntax for creating the website (the website will be creating using the components (functional based ), than calling the function to render the markup on the screen )
+
+// Whenever there is content on the website that is dynamic in nature => that content should be wrapped inside the state variable so that React will intiate the re-render / refresh that website when the content changes. 
+
+// React only updates that content of the realDOM that has been updated and hence updates the real DOM efficiently.
+
+// In react Through components the website is created, components handles the content => In componets we will handle the state
